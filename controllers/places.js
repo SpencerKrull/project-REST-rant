5193861dbe51df769896a08a818a9deece125ec2
Spencer Krull <spencerkrull138@gmail.com>
2022-06-12 11:51:39 -0600

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -1,4 +1,5 @@
 const router = require('express').Router()
+const res = require('express/lib/response')
 const places = require('../models/places.js')
 
 router.get('/new', (req, res) => {
@@ -22,7 +23,21 @@ router.get('/:id', (req, res) => {
     }
   })
 
-  router.delete('/places/:id', (req, res) => {
+router.get('/:id/edit', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+        res.render('error404')
+    }
+    else if (!places[id]) {
+        res.render('error404')
+    }
+    else {
+      res.render('places/edit', { place: places[id] })
+    }
+  })
+  
+
+router.delete('/places/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
       res.render('error404')
