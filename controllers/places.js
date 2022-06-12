fc2f175cdc30953660bcdeddf53d4092c6a253ea
Spencer Krull <spencerkrull138@gmail.com>
2022-06-12 11:40:17 -0600

    Delete route implemented - still buggy

--- controllers/places.js
+++ controllers/places.js
@@ -12,15 +12,29 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     let id = Number(req.params.id)
     if (isNaN(id)) {
-        res.render('error404')
+      res.render('error404')
     }
     else if (!places[id]) {
-        res.render('error404')
+      res.render('error404')
     }
     else {
-    res.render('places/show', { place: places[id] })
+      res.render('places/show', { place: places[id], id })
     }
-})
+  })
+
+  router.delete('/places/:id', (req, res) => {
+    let id = Number(req.params.id)
+    if (isNaN(id)) {
+      res.render('error404')
+    }
+    else if (!places[id]) {
+      res.render('error404')
+    }
+    else {
+      places.splice(id, 1)
+      res.redirect('/places')
+    }
+  })
 
 router.post('/', (req, res) => {
     console.log(req.body)
