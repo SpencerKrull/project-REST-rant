16207a99c6e00b422bc252b920b961a12a2dcb13
Spencer Krull <spencerkrull138@gmail.com>
2022-06-12 10:12:41 -0600

    Navbar with style

--- views/home.jsx
+++ views/home.jsx
@@ -5,7 +5,7 @@ function home () {
     return (
         <Def>
             <main>
-                <h1>HOME</h1>
+                <h1>REST-RANT</h1>
                 <div>
                     <img src="/images/scream.jpg" alt="Screaming Dude" />
                     <div>
