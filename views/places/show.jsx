aa0a8398d21e2430b58ff2377af2b1681b14ea37
Spencer Krull <spencerkrull138@gmail.com>
2022-06-22 21:50:41 -0600

    added helper method

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -5,22 +5,26 @@ function show (data) {
     return (
         <Def>
             <main>
-                <h1>{ data.place.name }</h1>
-                <h2>{ data.place.city }</h2>
-                <h2>{ data.place.state }</h2>
-                <h2>{ data.place.cuisine }</h2>
-                <h2>{ data.place.founded }</h2>
-                <h2>currently unrated</h2>
+            <main>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>Located in {data.place.city}, {data.place.state}</h3>
+                    </div>
+                </div>
+                <div className="col-sm-6">
+                    <h2>Description</h2>
+                    <h3>{data.place.showEstablished()}</h3>
+                    <h4>Serving {data.place.cuisines}</h4>
+                </div>
+            </main>
+                <h2>Currently Unrated</h2>
                 <h2>No comments yet!</h2>
             </main>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> 
-                Edit
-            </a>     
+            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a>     
 
             <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
-                <button type="submit" className="btn btn-danger">
-                    Delete
-                </button>
+                <button type="submit" className="btn btn-danger">Delete</button>
             </form> 
         </Def>
     )
