c945148d33c39e0832286a19b10b72a1342e9026
Spencer Krull <spencerkrull138@gmail.com>
2022-06-23 20:19:47 -0600

    populated comments

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -2,6 +2,25 @@ const React = require('react')
 const Def = require('../default')
 
 function show (data) {
+    let comments = (
+        <h3 className="inactive">
+            No comments yet!
+        </h3>
+    )
+    if (data.place.comments.length) {
+        comments = data.place.comments.map(c => {
+            return (
+                <div className="border">
+                    <h2 className="rant">{c.rant ? 'Rant! >:(' : 'Rave! :-)'}</h2>
+                    <h4>{c.content}</h4>
+                    <h3>
+                        <strong>- {c.author}</strong>
+                    </h3>
+                    <h4>Rating: {c.stars}</h4>
+                </div>
+            )
+        })
+    }
     return (
         <Def>
             <main>
@@ -16,8 +35,10 @@ function show (data) {
                     <h3>{data.place.showEstablished()}</h3>
                     <h4>Serving {data.place.cuisines}</h4>
                 </div>
+                <hr />
+                <h2>Comments</h2>
+                {comments}
                 <h2>Currently Unrated</h2>
-                <h2>No comments yet!</h2>
             </main>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a>     
 
