bb25ca9ad85ff4e9e4f1630e139ad0aee736c9da
Spencer Krull <spencerkrull138@gmail.com>
2022-06-23 21:00:06 -0600

    created comments successfully - except only on Food Time?

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -3,9 +3,7 @@ const Def = require('../default')
 
 function show (data) {
     let comments = (
-        <h3 className="inactive">
-            No comments yet!
-        </h3>
+        <h3 className="inactive">No comments yet!</h3>
     )
     if (data.place.comments.length) {
         comments = data.place.comments.map(c => {
@@ -24,13 +22,13 @@ function show (data) {
     return (
         <Def>
             <main>
-                <div className="row">
+                <div className="row border-top">
                     <div className="col-sm-6">
                         <img src={data.place.pic} alt={data.place.name} />
                         <h3>Located in {data.place.city}, {data.place.state}</h3>
                     </div>
                 </div>
-                <div className="col-sm-6">
+                <div className="col-sm-6 justify-content-right mt-5 border-top">
                     <h2>Description</h2>
                     <h3>{data.place.showEstablished()}</h3>
                     <h4>Serving {data.place.cuisines}</h4>
@@ -38,6 +36,30 @@ function show (data) {
                 <hr />
                 <h2>Comments</h2>
                 {comments}
+
+                <form method='POST' action={`/places/${data.place.id}/comment`}>
+                  <div className='form-group'>
+                    <label htmlFor='author'>Author</label>
+                    <input className='form-control' id='author' name='author'/>
+                  </div>
+
+                  <div className='form-group'>
+                    <label htmlFor='content'>Content</label>
+                    <input className='form-control' id='content' name='content' type='textarea' />
+                  </div>
+
+                  <div className='form-group'>
+                    <label htmlFor='stars'>Star Rating</label>
+                    <input className='form-control' id='stars' name='stars' type='range' step='0.5' min='0' max='5' />
+                  </div>
+
+                  <div className='form-group'>
+                    <label htmlFor='rant'>Rant</label>
+                    <input  id='rant' name='rant' type='checkbox' defaultChecked/>
+                  </div>
+
+                  <input className='btn btn-primary' type='submit' value='Add Comment' />
+                </form>
                 <h2>Currently Unrated</h2>
             </main>
             <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a>     
