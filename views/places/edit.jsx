4d13cbb6aae37b592184255c7e5bbd62cb9da4b4
Spencer Krull <spencerkrull138@gmail.com>
2022-06-27 23:31:34 -0600

    Reimplemented PUT and DELETE

--- views/places/edit.jsx
+++ views/places/edit.jsx
@@ -6,7 +6,7 @@ function edit_form (data) {
         <Def>
             <main>
                 <h1>Edit Places</h1>
-                <form method="POST" action={`/places/${data.id}?_method=PUT`}>
+                <form method="POST" action={`/places/${data.place.id}?_method=PUT`}>
                     <div className="row">
                         <div className="form-group col-sm-6">
                             <label htmlFor="name">Place Name</label>
@@ -29,8 +29,8 @@ function edit_form (data) {
                             <input className="form-control" id="cuisines" name="cuisines" required/>
                         </div>
                         <div className="form-group">
-                            <label for="founded">Founded Year</label>
-                            <input className="form-control" id="founded" name="founded" />
+                            <label htmlFor="founded">Founded</label>
+                            <input className="form-control" id="founded" name="founded" value={data.place.founded} />
                         </div>
                     </div>
                     <input className="btn btn-primary" type="submit" value="Edit Place" />
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -86,9 +86,9 @@ function show (data) {
                 </form>
                 <h2>Currently Unrated</h2>
             </main>
-            <a href={`/places/${data.id}/edit`} className="btn btn-warning"> Edit</a>     
+            <a href={`/places/${data.place.id}/edit`} className="btn btn-warning"> Edit</a>     
 
-            <form method="POST" action={`/places/${data.id}?_method=DELETE`}> 
+            <form method="POST" action={`/places/${data.place.id}?_method=DELETE`}> 
                 <button type="submit" className="btn btn-danger">Delete</button>
             </form> 
         </Def>
